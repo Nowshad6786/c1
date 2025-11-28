5d501b4049c8f538936a4039ea60cfc0449b428e
Nowshad6786 <panduchinni786@gmail.com>
2025-11-28 20:16:52 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int number;
+printf("Enter a number:");
+scanf("%d",&number);
+if(number>=100){
+printf("Number is greater than 100");
+}
+else{
+printf("Num is not greater than 100");
+}return 0;
+}
